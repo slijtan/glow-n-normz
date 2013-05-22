77466d61e6ee115c1dab9a6bc0a0a90a263718c2
Lijen Tan <lijentan@gmail.com>
2013-05-22 11:56:27 -0700

    updated formatting with fonts and some sprites, diagonal top and bottom borders, very rough layout with titles and content

--- js/custom.js
+++ js/custom.js
@@ -1,10 +1,26 @@
-function setFullScreen(){
+function setDivHeight(){
     $screen_height = $(window).height();
-    $('.full-screen').each(function(index) {
-	$(this).css('height', $screen_height);
+    $('div.page').each(function() {
+	if($(this).data('height')){
+	    $height = $(this).data('height');
+	    $height = (Number($height.replace('%', '')) * $screen_height / 100) + "px";
+	    $(this).css("height", $height);
+	}
+    });
+}
+
+function setDiagonals(){
+    $screen_width = $(window).width();
+
+    $('div.diagonal').each(function() {
+	$ratio = $(this).data('ratio');
+	$height = $screen_width * $ratio;
+	$(this).css("border-right-width", $screen_width + "px");
+	$(this).css("border-top-width", $height + "px");
     });
 }
 
 $(function() {
-    setFullScreen();
+    setDivHeight();
+    setDiagonals();
 });
