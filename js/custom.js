4b4542edce8a1781cb92bc26dc56246397d7b6cb
Lijen Tan <lijentan@gmail.com>
2013-05-22 14:26:16 -0700

    title page is mostly working, just gotta get the fade out, added all nav links

--- js/custom.js
+++ js/custom.js
@@ -20,7 +20,61 @@ function setDiagonals(){
     });
 }
 
+function initAnimations(){
+    var controller = $.superscrollorama();
+
+    //WELCOME PAGE
+    controller.addTween('#welcome-text',
+			(new TimelineLite())
+			.append([
+			    TweenMax.fromTo($('#flower-1'), 1,
+					    {css:{top: 50}, immediateRender:true},
+					    {css:{top: 200}}),
+			    TweenMax.fromTo($('#flower-2'), 1,
+					    {css:{bottom: 0}, immediateRender:true},
+					    {css:{bottom: 550}}),
+			    TweenMax.fromTo($('#flower-3'), 1,
+					    {css:{top: -200}, immediateRender:true},
+					    {css:{top: 300}})
+			]),
+			1000 // scroll duration of tween
+		       );
+
+    //ABOUT US TITLE
+    controller.addTween('#title-1-bg',
+			TweenMax.from($('#title-1-bg'), .5, {css:{opacity:0}}),
+			600,
+			300
+		       );
+
+    controller.addTween('#title-1-header',
+			TweenMax.from($('#title-1-header'), .5, {css:{opacity:0}}),
+			300,
+			-300
+		       );
+
+    controller.pin($('#title-1-header'), 1800, {offset: -350});
+
+    //ABOUT US
+    controller.addTween('#how-we-met',
+			TweenMax.from($('#how-we-met'), .5, {css:{opacity:0}}),
+			400,
+			-400
+		       );
+    controller.pin($('#how-we-met'), 700, {offset: -400});
+
+    controller.addTween('#the-proposal',
+			TweenMax.from($('#the-proposal'), .5, {css:{opacity:0}}),
+			400,
+			-200
+		       );
+
+    controller.pin($('#the-proposal'), 700, {offset: -100});
+
+}
+
 $(function() {
     setDivHeight();
     setDiagonals();
+    initAnimations();
 });
