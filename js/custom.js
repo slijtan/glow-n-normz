931e65be90056b2fcd761f15ea872a41ecb7d3bb
Lijen Tan <lijentan@gmail.com>
2013-05-29 00:41:48 -0700

    fixing timing of event details

--- js/custom.js
+++ js/custom.js
@@ -103,21 +103,21 @@ function initAnimations(){
     controller.addTween('#event',
 			TweenMax.from($('#event'), .5, {css:{opacity:0}}),
 			400,
-			-200
+			0
 		       );
-    controller.pin($('#event'), 1200, {offset: -100, pushFollowers: false});
+    controller.pin($('#event'), 1102, {offset: 200, pushFollowers: false});
 
     controller.addTween('#map',
 			TweenMax.from($('#map'), .5, {css:{opacity:0}}),
 			400,
 			0
 		       );
-    controller.pin($('#map'), 765, {offset: 30, pushFollowers: false});
+    controller.pin($('#map'), 700, {offset: 30, pushFollowers: false});
 
     controller.addTween('#directions',
 			TweenMax.from($('#directions'), .5, {css:{opacity:0}}),
-			400,
-			-100
+			200,
+			100
 		       );
     controller.pin($('#directions'), 230, {offset: -100, pushFollowers: false});
 
